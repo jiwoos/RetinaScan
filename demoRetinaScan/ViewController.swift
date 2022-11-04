2e960503c9da2924af80ecd41fc41b46e0f93fe5
Jiwoo Seo <jiwooseo@wustl.edu>
2022-11-03 23:47:28 -0500

    Initial Commit

---
+++ demoRetinaScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoRetinaScan
+//
+//  Created by Jiwoo Seo on 11/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
