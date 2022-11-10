29cf991fe0a2605c456c29fdcbfd201663433914
Jiwoo Seo <jiwooseo@wustl.edu>
2022-11-10 01:40:07 -0600

    developer mode and background changes

---
+++ demoRetinaScan/ARSCNViewDelegate.swift
@@ -0,0 +1,92 @@
+//
+//  ARSCNViewDelegate.swift
+//  demoRetinaScan
+//
+//  Created by Jiwoo Seo on 11/3/22.
+//
+
+import Foundation
+import CoreGraphics
+import SceneKit
+import ARKit
+
+//--------------------------
+// MARK: - ARSCNViewDelegate
+//--------------------------
+
+extension ViewController: ARSCNViewDelegate{
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+
+        // Setting up the face and eyes
+        faceNode = node
+        faceNode.addChildNode(leftEye)
+        faceNode.addChildNode(rightEye)
+        faceNode.transform = node.transform
+
+        
+        trackDistance()
+//        setObjectSize()
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+
+        faceNode.transform = node.transform
+
+        // Check if we have a valid ARFaceAnchor
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        
+        if faceAnchor.isTracked {
+            addObject()
+        } else {
+            deleteObject()
+        }
+        // Update the Transform of the left and right eyes
+        leftEye.simdTransform = faceAnchor.leftEyeTransform
+        rightEye.simdTransform = faceAnchor.rightEyeTransform
+
+        
+        trackDistance()
+//        setObjectSize()
+    }
+
+    func addObject() {
+        DispatchQueue.main.async {
+            self.objectView.isHidden = false
+            self.uiViewBackground.backgroundColor = UIColor.lightGray
+        }
+    }
+    
+    func deleteObject() {
+        DispatchQueue.main.async {
+            self.uiViewBackground.backgroundColor = UIColor.red
+            self.objectView.isHidden = true
+        }
+       
+    }
+    // Tracks the distance of the eyes from the camera
+    func trackDistance(){
+
+        DispatchQueue.main.async {
+
+            // Get the distance the eyes from the camera
+            let leftEyeDistanceFromCamera = self.leftEye.worldPosition - SCNVector3Zero
+            let rightEyeDistanceFromCamera = self.rightEye.worldPosition - SCNVector3Zero
+
+            // Calculate the average distance
+            let averageDistance = (leftEyeDistanceFromCamera.length() + rightEyeDistanceFromCamera.length()) / 2
+            let averageDistanceCM = (Int(round(averageDistance * 100)))
+            print("Approximate distance from the camera = \(averageDistanceCM)")
+        }
+    }
+    
+    func setObjectSize() {
+        DispatchQueue.main.async {
+            let newWidth = self.objectView.frame.width + 1
+            let newHeight = self.objectView.frame.height + 1
+            
+            self.objectView.frame = CGRectMake (self.objectView.frame.origin.x, self.objectView.frame.origin.y, newWidth, newHeight)
+        }
+    }
+    
+}
--- demoRetinaScan/ViewController.swift
+++ demoRetinaScan/ViewController.swift
@@ -6,14 +6,82 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var objectView: UIImageView!
+    @IBOutlet weak var modeController: UISegmentedControl!
+    @IBOutlet weak var uiViewBackground: UIView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    var faceNode = SCNNode()
+    var leftEye = SCNNode()
+    var rightEye = SCNNode()
+
+    //-----------------------
+    // MARK: - View LifeCycle
+    //-----------------------
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        objectView.image = UIImage(named:"foo")
+
+        // Set up face tracking
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+
+        
+        setupEyeNode()
     }
 
+    override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)
+        
+    }
+
+    //-----------------------
+    // MARK: - Eye Node Setup
+    //-----------------------
+
+    // Creates to SCNSpheres to loosely represent the eyes
+    func setupEyeNode(){
+
+        // Create a node to represent the eye
+        let eyeGeometry = SCNSphere(radius: 0.005)
+        eyeGeometry.materials.first?.diffuse.contents = UIColor.cyan
+        eyeGeometry.materials.first?.transparency = 1
+
+        // Create a holder node and rotate it
+        // so that the geometry points towards the device
+        let node = SCNNode()
+        node.geometry = eyeGeometry
+        node.eulerAngles.x = -.pi / 2
+        node.position.z = 0.1
+
+        // Create left and right eye
+        leftEye = node.clone()
+        rightEye = node.clone()
+    }
+    
+
+    @IBAction func modeChanged(_ sender: Any) {
+        switch modeController.selectedSegmentIndex
+            {
+            case 0:
+                sceneView.isHidden = true
+            case 1:
+                sceneView.isHidden = false
+            default:
+                break
+            }
+    }
+    
 
 }
 
+    
